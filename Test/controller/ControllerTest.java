76119efe593856f4c42d0e62f91ac1bb133cb6b5
drewl <andrewahaz@outlook.dk>
2024-03-20 08:49:47 +0100

    controller done

---
+++ Test/controller/ControllerTest.java
@@ -0,0 +1,40 @@
+package controller;
+
+import ordination.PN;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    Controller controller = Controller.getController();
+
+    @Test
+    void opretPNOrdination() {
+
+
+
+    }
+
+    @Test
+    void opretDagligFastOrdination() {
+    }
+
+    @Test
+    void opretDagligSkaevOrdination() {
+    }
+
+    @Test
+    void ordinationPNAnvendt() {
+    }
+
+    @Test
+    void anbefaletDosisPrDoegn() {
+    }
+
+    @Test
+    void antalOrdinationerPrVægtPrLægemiddel() {
+    }
+}
