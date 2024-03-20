18ada15840c3623177dde36b06e6312c5811017b
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-20 11:47:15 +0100

    added opretDagligSkaevOrdinationUgyldig01

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -239,6 +239,28 @@ class ControllerTest {
         assertTrue(patient.getOrdinationer().contains(actual));
     }
     @Test
+    void opretDagligSkaevOrdinationUgyldig01() {
+        Patient patient = new Patient("123456-7890", "Anders Hansen", 70);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 2, 5, 10, "Pille");
+        LocalTime[] klokkeslet = {LocalTime.of(9, 0),LocalTime.of(12, 0),LocalTime.of(15, 0),LocalTime.of(18, 0)};
+        double[] antalEnheder = {1, 2, 1};
+        DagligSkaev actual = controller.opretDagligSkaevOrdination(
+                LocalDate.of(2024,3, 18),
+                LocalDate.of(2024,3, 20),
+                patient,
+                laegemiddel,
+                klokkeslet,
+                antalEnheder
+        );
+        assertTrue(antalEnheder.length == klokkeslet.length);
+        assertTrue(actual.getStartDen().isBefore(actual.getSlutDen()));
+        // Input er oprettet
+        assertNotNull(patient);
+        assertEquals(laegemiddel, actual.getLaegemiddel());
+        assertTrue(patient.getOrdinationer().contains(actual));
+    }
+
+    @Test
     @DisplayName("Normal test")
     void ordinationPNAnvendt1() {
 
