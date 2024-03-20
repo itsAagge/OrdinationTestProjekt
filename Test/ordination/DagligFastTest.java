720477efaf4b0fdb4d0c7292452c6caff7da0b90
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-20 13:31:36 +0100

    added doegnDosis

--- Test/ordination/DagligFastTest.java
+++ Test/ordination/DagligFastTest.java
@@ -14,12 +14,21 @@ class DagligFastTest {
         Laegemiddel middel = new Laegemiddel("Paracetamol",2, 2, 2, "Pille");
         DagligFast ordination = controller.opretDagligFastOrdination(LocalDate.of(2024,7,13),LocalDate.of(2024,7,19), pas, middel,1,1,1,1);
         assertEquals(28, ordination.samletDosis());
-
-
+    }
+    @Test
+    void samletDosisSammeDag() {
+        Patient pas = new Patient("123456-7890", "Anders Handsen", 85);
+        Laegemiddel middel = new Laegemiddel("Paracetamol",2, 2, 2, "Pille");
+        DagligFast ordination = controller.opretDagligFastOrdination(LocalDate.of(2024,7,19),LocalDate.of(2024,7,19), pas, middel,1,1,1,1);
+        assertEquals(4, ordination.samletDosis());
     }
 
     @Test
     void doegnDosis() {
+        Patient pas = new Patient("123456-7890", "Anders Handsen", 85);
+        Laegemiddel middel = new Laegemiddel("Paracetamol",2, 2, 2, "Pille");
+        DagligFast ordination = controller.opretDagligFastOrdination(LocalDate.of(2024,7,13),LocalDate.of(2024,7,19), pas, middel,1,1,1,1);
+        assertEquals(4, ordination.doegnDosis());
 
     }
 }
