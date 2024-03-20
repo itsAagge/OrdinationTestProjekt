9e54e2d73dd12f0f326f9b376a84d194a1c127cc
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-20 09:59:20 +0100

    made a test for opretDagligSkaevOrdination

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,9 +1,13 @@
 package controller;
 
+import ordination.DagligSkaev;
+import ordination.Laegemiddel;
 import ordination.PN;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +28,24 @@ class ControllerTest {
 
     @Test
     void opretDagligSkaevOrdination() {
+        Patient patient = new Patient("123456-7890", "Anders Hansen", 70);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 2, 5, 10, "Pille");
+        LocalTime[] klokkeslet = {LocalTime.of(9, 0),LocalTime.of(12, 0),LocalTime.of(15, 0),LocalTime.of(18, 0)};
+        double[] antalEnheder = {1, 2, 1, 2};
+        DagligSkaev actual = controller.opretDagligSkaevOrdination(
+            LocalDate.of(2024,3, 18),
+            LocalDate.of(2024,3, 20),
+            patient,
+            laegemiddel,
+            klokkeslet,
+            antalEnheder
+        );
+            assertTrue(antalEnheder.length == klokkeslet.length);
+            assertTrue(actual.getStartDen().isBefore(actual.getSlutDen()));
+            // Input er oprettet
+            assertNotNull(patient);
+            assertEquals(laegemiddel, actual.getLaegemiddel());
+            assertTrue(patient.getOrdinationer().contains(actual));
     }
 
     @Test
