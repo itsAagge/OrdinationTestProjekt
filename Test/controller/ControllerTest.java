4a3e370e671b622bfd7e937dd8ceb903e7db7d55
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-03-20 12:03:29 +0100

    added Ugyldige tests

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -197,7 +197,7 @@ class ControllerTest {
         assertTrue(patient.getOrdinationer().contains(actual));
     }
     @Test
-    void opretDagligSkaevOrdination02() {
+    void opretDagligSkaevOrdination02()  {
         Patient patient = new Patient("123456-7890", "Anders Hansen", 70);
         Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 2, 5, 10, "Pille");
         LocalTime[] klokkeslet = {LocalTime.of(9, 0),LocalTime.of(12, 0),LocalTime.of(15, 0),LocalTime.of(18, 0)};
@@ -211,7 +211,7 @@ class ControllerTest {
                 antalEnheder
         );
         assertTrue(antalEnheder.length == klokkeslet.length);
-        assertTrue(actual.getStartDen().isBefore(actual.getSlutDen()));
+        assertTrue(actual.getStartDen().isBefore(actual.getSlutDen()) || actual.getStartDen().equals(actual.getSlutDen()));
         // Input er oprettet
         assertNotNull(patient);
         assertEquals(laegemiddel, actual.getLaegemiddel());
@@ -247,6 +247,27 @@ class ControllerTest {
         DagligSkaev actual = controller.opretDagligSkaevOrdination(
                 LocalDate.of(2024,3, 18),
                 LocalDate.of(2024,3, 20),
+                patient,
+                laegemiddel,
+                klokkeslet,
+                antalEnheder
+        );
+        assertTrue(antalEnheder.length == klokkeslet.length);
+        assertTrue(actual.getStartDen().isBefore(actual.getSlutDen()));
+        // Input er oprettet
+        assertNotNull(patient);
+        assertEquals(laegemiddel, actual.getLaegemiddel());
+        assertTrue(patient.getOrdinationer().contains(actual));
+    }
+    @Test
+    void opretDagligSkaevOrdinationUgyldig02() {
+        Patient patient = new Patient("123456-7890", "Anders Hansen", 70);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 2, 5, 10, "Pille");
+        LocalTime[] klokkeslet = {LocalTime.of(9, 0),LocalTime.of(12, 0),LocalTime.of(15, 0),LocalTime.of(18, 0)};
+        double[] antalEnheder = {1, 2, 1};
+        DagligSkaev actual = controller.opretDagligSkaevOrdination(
+                LocalDate.of(2024,3, 18),
+                null,
                 patient,
                 laegemiddel,
                 klokkeslet,
