e42109abe65f825e79eb4a22824e053a247d2cdf
August Juel Aagaard <augustjaagaard@gmail.com>
2024-03-20 11:31:15 +0100

    Created tests for antalOrdinationerPrVægtPrLægemiddel()

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,9 +1,6 @@
 package controller;
 
-import ordination.DagligFast;
-import ordination.Laegemiddel;
-import ordination.PN;
-import ordination.Patient;
+import ordination.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +15,8 @@ class ControllerTest {
     @Test
     void opretPNOrdination1() {
 
-        Patient AndersHansen = new Patient("123456-7890", "Anders Hansen", 80.00);
-        Laegemiddel parcetamol = new Laegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
+        Patient AndersHansen = controller.opretPatient("123456-7890", "Anders Hansen", 80.00);
+        Laegemiddel parcetamol = controller.opretLaegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
         PN actual = controller.opretPNOrdination(LocalDate.of(2024,03,18), LocalDate.of(2024, 03, 20), AndersHansen, parcetamol, 10);
 
         assertEquals(LocalDate.of(2024,03,18), actual.getStartDen());
@@ -35,8 +32,8 @@ class ControllerTest {
     @Test
     void opretPNOrdination2() {
 
-        Patient AndersHansen = new Patient("123456-7890", "Anders Hansen", 80.00);
-        Laegemiddel parcetamol = new Laegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
+        Patient AndersHansen = controller.opretPatient("123456-7890", "Anders Hansen", 80.00);
+        Laegemiddel parcetamol = controller.opretLaegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
         PN actual = controller.opretPNOrdination(LocalDate.of(2024,03,18), LocalDate.of(2024, 03, 20), AndersHansen, parcetamol, 1);
 
         assertEquals(LocalDate.of(2024,03,18), actual.getStartDen());
@@ -51,8 +48,8 @@ class ControllerTest {
     @Test
     void opretPNOrdination3() {
 
-        Patient AndersHansen = new Patient("123456-7890", "Anders Hansen", 80.00);
-        Laegemiddel parcetamol = new Laegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
+        Patient AndersHansen = controller.opretPatient("123456-7890", "Anders Hansen", 80.00);
+        Laegemiddel parcetamol = controller.opretLaegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
         PN actual = controller.opretPNOrdination(LocalDate.of(2024,03,22), LocalDate.of(2024, 03, 22), AndersHansen, parcetamol, 10);
 
         assertEquals(LocalDate.of(2024,03,22), actual.getStartDen());
@@ -68,8 +65,8 @@ class ControllerTest {
     @Test
     void opretPNOrdination4() {
 
-        Patient AndersHansen = new Patient("123456-7890", "Anders Hansen", 80.00);
-        Laegemiddel parcetamol = new Laegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
+        Patient AndersHansen = controller.opretPatient("123456-7890", "Anders Hansen", 80.00);
+        Laegemiddel parcetamol = controller.opretLaegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
         PN actual = controller.opretPNOrdination(LocalDate.of(2024,03,22), LocalDate.of(2024, 03, 22), AndersHansen, parcetamol, 10);
 
         Throwable excepction = assertThrows(IllegalArgumentException.class, () -> controller.opretPNOrdination(LocalDate.of(2024,03,20), LocalDate.of(2024, 03, 18), AndersHansen, parcetamol, 10));
@@ -79,8 +76,8 @@ class ControllerTest {
     @Test
     void opretPNOrdination5() {
 
-        Patient AndersHansen = new Patient("123456-7890", "Anders Hansen", 80.00);
-        Laegemiddel parcetamol = new Laegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
+        Patient AndersHansen = controller.opretPatient("123456-7890", "Anders Hansen", 80.00);
+        Laegemiddel parcetamol = controller.opretLaegemiddel("Paracetamol", 0.1,0.2,0.4, "styk");
         PN actual = controller.opretPNOrdination(LocalDate.of(2024,03,22), LocalDate.of(2024, 03, 22), AndersHansen, parcetamol, 10);
 
         Throwable excepction = assertThrows(IllegalArgumentException.class, () -> controller.opretPNOrdination(LocalDate.of(2024,03,18), LocalDate.of(2024, 03, 20), AndersHansen, parcetamol, -1));
@@ -91,8 +88,8 @@ class ControllerTest {
     @Test
     @DisplayName("DagligFast Base")
     void opretDagligFastOrdination1() {
-        Patient patient = new Patient("123456-7890", "Anders Hansen", 80);
-        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
+        Patient patient = controller.opretPatient("123456-7890", "Anders Hansen", 80);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
         LocalDate startDen = LocalDate.of(2024, 03, 18);
         LocalDate slutDen = LocalDate.of(2024, 03, 20);
 
@@ -107,8 +104,8 @@ class ControllerTest {
     @Test
     @DisplayName("DagligFast antalMiddag = 0")
     void opretDagligFastOrdination2() {
-        Patient patient = new Patient("123456-7890", "Anders Hansen", 80);
-        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
+        Patient patient = controller.opretPatient("123456-7890", "Anders Hansen", 80);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
         LocalDate startDen = LocalDate.of(2024, 03, 18);
         LocalDate slutDen = LocalDate.of(2024, 03, 20);
 
@@ -123,8 +120,8 @@ class ControllerTest {
     @Test
     @DisplayName("DagligFast samme dag")
     void opretDagligFastOrdination3() {
-        Patient patient = new Patient("123456-7890", "Anders Hansen", 80);
-        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
+        Patient patient = controller.opretPatient("123456-7890", "Anders Hansen", 80);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
         LocalDate startDen = LocalDate.of(2024, 03, 22);
         LocalDate slutDen = LocalDate.of(2024, 03, 22);
 
@@ -139,8 +136,8 @@ class ControllerTest {
     @Test
     @DisplayName("DagligFast fejl")
     void opretDagligFastOrdination4() {
-        Patient patient = new Patient("123456-7890", "Anders Hansen", 80);
-        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
+        Patient patient = controller.opretPatient("123456-7890", "Anders Hansen", 80);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.4, "Styk");
         LocalDate startDen = LocalDate.of(2024, 03, 20);
         LocalDate slutDen = LocalDate.of(2024, 03, 18);
 
@@ -162,6 +159,78 @@ class ControllerTest {
     }
 
     @Test
-    void antalOrdinationerPrVægtPrLægemiddel() {
+    @DisplayName("Ordinationer pr vægt - 65-100 Wegovy")
+    void antalOrdinationerPrVægtPrLægemiddel1() {
+        Laegemiddel paracetamol = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.3, "Styk");
+        Laegemiddel wegovy = controller.opretLaegemiddel("Wegovy", 0.1, 0.2, 0.3, "Styk");
+        Patient anders = controller.opretPatient("123456-7890", "Anders Hansen", 20);
+        Patient peter = controller.opretPatient("123456-7890", "Peter Hansen", 80);
+        Patient mogens = controller.opretPatient("123456-7890", "Mogens Hansen", 140);
+        Patient nils = controller.opretPatient("123456-7890", "Nils Værker", 20);
+        Ordination andersOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), anders, wegovy, 1, 1, 1, 1);
+        Ordination peterOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), peter, wegovy, 1, 1, 1, 1);
+        Ordination mogensOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), mogens, paracetamol, 1, 1, 1, 1);
+        Ordination nilsOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), nils, paracetamol, 1, 1, 1, 1);
+
+        int actual = controller.antalOrdinationerPrVægtPrLægemiddel(65, 100, wegovy);
+
+        assertEquals(1, actual);
+    }
+
+    @Test
+    @DisplayName("Ordinationer pr vægt - 1-200 Wegovy")
+    void antalOrdinationerPrVægtPrLægemiddel2() {
+        Laegemiddel paracetamol = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.3, "Styk");
+        Laegemiddel wegovy = controller.opretLaegemiddel("Wegovy", 0.1, 0.2, 0.3, "Styk");
+        Patient anders = controller.opretPatient("123456-7890", "Anders Hansen", 20);
+        Patient peter = controller.opretPatient("123456-7890", "Peter Hansen", 80);
+        Patient mogens = controller.opretPatient("123456-7890", "Mogens Hansen", 140);
+        Patient nils = controller.opretPatient("123456-7890", "Nils Værker", 20);
+        Ordination andersOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), anders, wegovy, 1, 1, 1, 1);
+        Ordination peterOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), peter, wegovy, 1, 1, 1, 1);
+        Ordination mogensOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), mogens, paracetamol, 1, 1, 1, 1);
+        Ordination nilsOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), nils, paracetamol, 1, 1, 1, 1);
+
+        int actual = controller.antalOrdinationerPrVægtPrLægemiddel(1, 200, wegovy);
+
+        assertEquals(2, actual);
+    }
+
+    @Test
+    @DisplayName("Ordinationer pr vægt - 20-100 Paracetamol")
+    void antalOrdinationerPrVægtPrLægemiddel3() {
+        Laegemiddel paracetamol = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.3, "Styk");
+        Laegemiddel wegovy = controller.opretLaegemiddel("Wegovy", 0.1, 0.2, 0.3, "Styk");
+        Patient anders = controller.opretPatient("123456-7890", "Anders Hansen", 20);
+        Patient peter = controller.opretPatient("123456-7890", "Peter Hansen", 80);
+        Patient mogens = controller.opretPatient("123456-7890", "Mogens Hansen", 140);
+        Patient nils = controller.opretPatient("123456-7890", "Nils Værker", 20);
+        Ordination andersOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), anders, wegovy, 1, 1, 1, 1);
+        Ordination peterOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), peter, wegovy, 1, 1, 1, 1);
+        Ordination mogensOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), mogens, paracetamol, 1, 1, 1, 1);
+        Ordination nilsOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), nils, paracetamol, 1, 1, 1, 1);
+
+        int actual = controller.antalOrdinationerPrVægtPrLægemiddel(20, 100, paracetamol);
+
+        assertEquals(1, actual);
+    }
+
+    @Test
+    @DisplayName("Ordinationer pr vægt - 1-200 Paracetamol")
+    void antalOrdinationerPrVægtPrLægemiddel4() {
+        Laegemiddel paracetamol = controller.opretLaegemiddel("Paracetamol", 0.1, 0.2, 0.3, "Styk");
+        Laegemiddel wegovy = controller.opretLaegemiddel("Wegovy", 0.1, 0.2, 0.3, "Styk");
+        Patient anders = controller.opretPatient("123456-7890", "Anders Hansen", 20);
+        Patient peter = controller.opretPatient("123456-7890", "Peter Hansen", 80);
+        Patient mogens = controller.opretPatient("123456-7890", "Mogens Hansen", 140);
+        Patient nils = controller.opretPatient("123456-7890", "Nils Værker", 20);
+        Ordination andersOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), anders, wegovy, 1, 1, 1, 1);
+        Ordination peterOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), peter, wegovy, 1, 1, 1, 1);
+        Ordination mogensOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), mogens, paracetamol, 1, 1, 1, 1);
+        Ordination nilsOrdination = controller.opretDagligFastOrdination(LocalDate.now(), LocalDate.now(), nils, paracetamol, 1, 1, 1, 1);
+
+        int actual = controller.antalOrdinationerPrVægtPrLægemiddel(1, 200, paracetamol);
+
+        assertEquals(2, actual);
     }
 }
